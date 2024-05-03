e3eaa083b02b3f492b6cd6d4781676475209e55b
Brian Cantrell <bcantrell@automatit.net>
2024-05-03 14:37:27 -0700

    Remove i18n

--- payload.config.ts
+++ payload.config.ts
@@ -10,7 +10,6 @@ import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import nodemailer from 'nodemailer'
 import path from 'path'
 import { buildConfig } from 'payload/config'
-import { en } from 'payload/i18n/en'
 import sharp from 'sharp'
 import { fileURLToPath } from 'url'
 
@@ -18,56 +17,53 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 const storageAdapter = s3Adapter({
-  config: {
-    region: process.env.S3_REGION,
-    forcePathStyle: true,
-    credentials: {
-      accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string
-    }
-  },
-  bucket: process.env.NEXT_PUBLIC_S3_BUCKET as string,
-  acl: 'public-read'
+	config: {
+		region: process.env.S3_REGION,
+		forcePathStyle: true,
+		credentials: {
+			accessKeyId: process.env.S3_ACCESS_KEY_ID as string,
+			secretAccessKey: process.env.S3_SECRET_ACCESS_KEY as string
+		}
+	},
+	bucket: process.env.NEXT_PUBLIC_S3_BUCKET as string,
+	acl: 'public-read'
 })
 
 export default buildConfig({
-  editor: lexicalEditor(),
-  globals: [siteSettings, Header, Footer],
-  collections: [users, pages, media, sessions],
-  secret: process.env.AUTH_SECRET || '',
-  typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts')
-  },
-  db: mongooseAdapter({
-    url: process.env.MONGODB_URI || ''
-  }),
-  email: nodemailerAdapter({
-    defaultFromAddress: 'donotreply@zr1tampa.com',
-    defaultFromName: 'Zr1 Tampa Admin',
-    transport: nodemailer.createTransport({
-      host: 'smtp.resend.com',
-      port: 465,
-      auth: {
-        user: 'resend',
-        pass: process.env.AUTH_RESEND_KEY
-      }
-    })
-  }),
-  i18n: {
-    supportedLanguages: { en }
-  },
-  plugins: [
-    cloudStorage({
-      collections: {
-        [COLLECTION_SLUG_MEDIA]: {
-          adapter: storageAdapter,
-          disablePayloadAccessControl: true,
-          generateFileURL: (args: any) => {
-            return `${process.env.NEXT_PUBLIC_S3_PUBLIC_URL}/${args.filename}`
-          }
-        }
-      }
-    })
-  ],
-  sharp
+	editor: lexicalEditor(),
+	globals: [siteSettings, Header, Footer],
+	collections: [users, pages, media, sessions],
+	secret: process.env.AUTH_SECRET || '',
+	typescript: {
+		outputFile: path.resolve(dirname, 'payload-types.ts')
+	},
+	db: mongooseAdapter({
+		url: process.env.MONGODB_URI || ''
+	}),
+	email: nodemailerAdapter({
+		defaultFromAddress: 'donotreply@zr1tampa.com',
+		defaultFromName: 'Zr1 Tampa Admin',
+		transport: nodemailer.createTransport({
+			host: 'smtp.resend.com',
+			port: 465,
+			auth: {
+				user: 'resend',
+				pass: process.env.AUTH_RESEND_KEY
+			}
+		})
+	}),
+	plugins: [
+		cloudStorage({
+			collections: {
+				[COLLECTION_SLUG_MEDIA]: {
+					adapter: storageAdapter,
+					disablePayloadAccessControl: true,
+					generateFileURL: (args: any) => {
+						return `${process.env.NEXT_PUBLIC_S3_PUBLIC_URL}/${args.filename}`
+					}
+				}
+			}
+		})
+	],
+	sharp
 })
