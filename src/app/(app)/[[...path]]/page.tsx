5580456f3e60ed4f9c92a8786c9c9e5ef077fd76
Brian Cantrell <bcantrell@automatit.net>
2024-05-05 23:20:58 -0700

    wrap page in useeffect

--- src/app/(app)/[[...path]]/page.tsx
+++ src/app/(app)/[[...path]]/page.tsx
@@ -1,7 +1,7 @@
 import Blocks from '@/components/Blocks'
 import { Hero } from '@/components/Hero'
 import fetchPage from '@/payload/utils/fetchPage'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
@@ -9,9 +9,15 @@ export const revalidate = 0
 const Page = async ({ params }: { params: { path: string[] } }) => {
   const page = await fetchPage(params.path)
 
+	useEffect(() => {
+		if(!page) {
+			return
+		}
+	},[page])
+
   return (
 		<React.Fragment>
-			{ page?.hero  ? <Hero {...page?.hero} /> : null}
+			<Hero {...page?.hero ||  <></>} />
 			{/* <Blocks blocks={page?.layout} locale="en" /> */}
 		</React.Fragment>
 	);
